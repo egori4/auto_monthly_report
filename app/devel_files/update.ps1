c5f38bb480742c79d8e1c43b1a4d6116715c340e
egori4 <egori4@gmail.com>
2023-08-15 09:33:12 -0400

    deleting the old repo

--- app/devel_files/update.ps1
+++
@@ -1,7 +0,0 @@
-..\..\tools\php\php -f ..\..\report.php -- --file="report.json" --monthText=July --monthDays=31 --month=07 --year=2021 --longName="Test Customer"
-if($?) {
-	..\..\tools\chrome\chrome --headless --incognito --enable-logging --print-to-pdf="C:\home\mockba\Report_v5.0\report_files\TEST\output.pdf" file://C:\home\mockba\Report_v5.0\report_files\TEST\report.htm
-} else {
-	write-host "Report not generated."
-	write-host ""
-}
